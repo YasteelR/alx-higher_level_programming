f141b45bec83057454eb01a60add38dc6b555f3a
yasteel <yasteel001@gmail.com>
2022-08-31 01:18:26 +0200

    first half done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
-#!/bin/python3
+#!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
     for character in my_string:
         new_str += character if character != "C" and character != "c" else ""
     return (new_str)
-    
