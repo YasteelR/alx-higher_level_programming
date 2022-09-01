ec11f871a5dc2e3e458201e2ea01ccfe6a63cca8
yasteel <yasteel001@gmail.com>
2022-09-01 04:17:32 +0200

    task 11 fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python3
-def helper(i):
-    if i is "I":
-        return 1
-    elif i is "V":
-        return 5
-    elif i is "X":
-        return 10
-    elif i is "L":
-        return 50
-    elif i is "C":
-        return 100
-    elif i is "D":
-        return 500
-    elif i is "M":
-        return 1000
-    else:
-        return 0
-
-
 def roman_to_int(roman_string):
-    count = 0
-    future = 0
-    number = 0
-
-    if type(roman_string) is not str or roman_string is None:
-        return 0
-    if len(roman_string) is 1:
-        return helper(roman_string[0])
-    for i in range(0, len(roman_string) - 1):
-        number = helper(roman_string[i])
-        future = helper(roman_string[i + 1])
-        if number >= future:
-            count += number
-        elif number < future:
-            count -= number
-    count += future
-    return count
-    
+    result = 0
+    if type(roman_string) == str:
+        largest = 1
+        rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for s in roman_string[::-1]:
+            if s in rn:
+                if rn[s] >= largest:
+                    result = result + rn[s]
+                    largest = rn[s]
+                else:
+                    result -= rn[s]
+    return result
